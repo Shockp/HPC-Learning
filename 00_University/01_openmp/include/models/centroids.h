dbc024bdcb78e8a7bb45f9efc4c328008b9dbd10
Adrián Feito Blázquez <59491201+Shockp@users.noreply.github.com>
2026-02-22 18:16:49 +0100

    Add Centroids class header file

---
+++ 00_University/01_openmp/include/models/centroids.h
@@ -0,0 +1,10 @@
+#ifndef UNIVERSITY_OPENMP_MODELS_DATASET_H_
+#define UNIVERSITY_OPENMP_MODELS_DATASET_H_
+
+namespace models {
+
+class Centroids {};
+
+} // namespace models
+
+#endif // UNIVERSITY_OPENMP_MODELS_DATASET_H_
