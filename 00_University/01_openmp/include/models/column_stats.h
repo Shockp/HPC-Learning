2fbb00af3a4659c5f0e87cb26c141876cb4036e8
Adrián Feito Blázquez <59491201+Shockp@users.noreply.github.com>
2026-02-22 18:16:38 +0100

    Add header file for Column_stats class

---
+++ 00_University/01_openmp/include/models/column_stats.h
@@ -0,0 +1,10 @@
+#ifndef UNIVERSITY_OPENMP_MODELS_DATASET_H_
+#define UNIVERSITY_OPENMP_MODELS_DATASET_H_
+
+namespace models {
+
+class Column_stats {};
+
+} // namespace models
+
+#endif // UNIVERSITY_OPENMP_MODELS_DATASET_H_
